2343d0e409b228794661cceaaa412cc18a4f6b4a
Tobias Zuber <tobiaszuber@Tobiass-MacBook-Pro.local>
2022-08-20 00:31:34 +0200

    organize folder struct. + add TODOs

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,20 @@ import 'package:split/my_game.dart';
 
 void main() {
   runApp(
-    GameWidget(
-      game: MyGame(),
-    ),
+    GameWidget(game: MyGame()),
   );
 }
+
+/*
+      TODO:
+        - draw maze(s)
+        - generate maze -> background + wall (path is clear)
+        - draw player(s)
+        - load sprite for player(s)
+        - impl. movement logic
+        - impl. collision logic
+        - impl. win/loose logic
+
+        nice to have:
+        - game menu (start, manual, credits, exit)
+ */
--- lib/my_game.dart
+++ lib/my_game.dart
@@ -1,11 +1,8 @@
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flame/input.dart';
-import 'package:split/square.dart';
+import 'package:split/component/square.dart';
 
-/// This example simply adds a rotating white square on the screen.
-/// If you press on a square, it will be removed.
-/// If you press anywhere else, another square will be added.
 class MyGame extends FlameGame with HasTappables {
   @override
   Future<void> onLoad() async {
