de35efbd6908ee03cad541fec6247f14fc31658a
Emma Lindmark <emma_lindmark@live.se>
2022-08-21 15:25:15 +0200

    Added ghost sprite

--- lib/component/real_player.dart
+++ lib/component/real_player.dart
@@ -1,5 +1,11 @@
+import 'package:flame/components.dart';
+import 'package:flame_mini_sprite/flame_mini_sprite.dart';
+import 'package:flutter/material.dart';
+import 'package:mini_sprite/mini_sprite.dart';
 import 'package:split/component/player.dart';
 import 'package:split/data/player_sprites_path.dart';
+
+import '../assets.dart';
 
 class RealPlayer extends Player {
   RealPlayer()
@@ -14,6 +20,43 @@ class RealPlayer extends Player {
   @override
   Future<void> onLoad() async {
     super.onLoad();
+    final miniLibrary = MiniLibrary.fromDataString(ghostLibrary);
+    final sprites = await miniLibrary.toSprites(
+      color: Colors.white,
+      pixelSize: 1,
+    );
+    animations = {
+      AnimationState.idle: SpriteAnimation.spriteList(
+        [
+          sprites['ghost_front1']!,
+        ],
+        stepTime: 0.2,
+      ),
+      AnimationState.movingSide: SpriteAnimation.spriteList(
+        [
+          sprites['ghost_side1']!,
+          sprites['ghost_side2']!,
+          sprites['ghost_side3']!,
+        ],
+        stepTime: 0.2,
+      ),
+      AnimationState.movingUp: SpriteAnimation.spriteList(
+        [
+          sprites['ghost_back1']!,
+          sprites['ghost_back2']!,
+          sprites['ghost_back3']!,
+        ],
+        stepTime: 0.2,
+      ),
+      AnimationState.movingDown: SpriteAnimation.spriteList(
+        [
+          sprites['ghost_front1']!,
+          sprites['ghost_front2']!,
+          sprites['ghost_front3']!,
+        ],
+        stepTime: 0.2,
+      ),
+    };
     position.addListener(() {
       gameRef.shadowPlayer.border.position = position;
     });
